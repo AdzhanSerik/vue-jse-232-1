91f28023bd75b83845d21c983689f796b4fff606
AdzhanSerik <adzhan.serik@gmail.com>
2024-07-04 20:06:16 +0500

    Point2

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,14 +1,26 @@
+<script setup>
+defineProps({
+  image: String,
+  name: String,
+  price: Number,
+  isAdded: Boolean,
+  isFavourite: Boolean,
+  onClickAdd: Function
+})
+</script>
+
+
 <template>
   <div class="border p-10 rounded-xl relative transition hover:-translate-y-2 hover:shadow-xl">
-    <img class="absolute top-5 left-5" src="/like-2.svg" alt="" />
-    <img src="/public/sneakers/sneakers-1.jpg" alt="" />
-    <p class="mt-5 mb-3">Мужские Кроссовки Nike Blazer Mid Suede</p>
+    <img :src="isFavourite ? '/like-2.svg' : '/like-1.svg'" class="absolute top-5 left-5" alt="" />
+    <img :src="image" alt="" />
+    <p class="mt-5 mb-3">{{ name }}</p>
     <div class="flex justify-between">
       <div class="flex flex-col">
         <span class="uppercase text-gray-400">цена:</span>
-        <span class="font-bold">12999 тенге</span>
+        <span class="font-bold">{{ price }} тенге</span>
       </div>
-      <img src="/plus.svg" alt="" />
+      <img @click="onClickAdd" :src="isAdded ? 'checked.svg' : 'plus.svg'" alt="" />
     </div>
   </div>
 </template>
--- src/components/CardsItems.vue
+++ src/components/CardsItems.vue
@@ -1,10 +1,21 @@
 <script setup>
 import Card from './CardItem.vue'
+
+const onClickAdd = () => {
+  alert('Добавлено')
+}
 </script>
 
 <template>
-  <div class="p-10 grid grid-cols-4 gap-5">
-    <Card />
+  <div class="p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5">
+    <Card
+      image="/sneakers/sneakers-1.jpg"
+      name="Мужские Кроссовки Nike Blazer Mid Suede"
+      :price="12999"
+      :isAdded="false"
+      :isFavourite="true"
+      :onClickAdd="onClickAdd"
+    />
     <Card />
     <Card />
     <Card />
