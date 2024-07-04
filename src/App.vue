dd26b7d73a7f6ce7730e9d7effe3666a11fff309
AdzhanSerik <adzhan.serik@gmail.com>
2024-07-04 21:00:37 +0500

    asdas

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from './components/HeaderComponent.vue'
 import Cards from './components/CardsItems.vue'
+import Drawer from './components/DrawerComponent.vue'
 </script>
 
 <template>
+  <Drawer />
   <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-14 pb-2">
     <Header />
     <h2 class="font-bold text-xl p-10">Все кроссовки</h2>
